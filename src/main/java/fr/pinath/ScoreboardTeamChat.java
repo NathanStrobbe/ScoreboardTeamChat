d0eab927830249cf0694d3454839e8d3b9420e9c
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2018-04-04 21:15:23 +0200

    Initial commit

---
+++ src/main/java/fr/pinath/ScoreboardTeamChat.java
@@ -0,0 +1,19 @@
+package fr.pinath;
+
+//import fr.pinath.listener.ChatListener;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScoreboardTeamChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info(getDescription().getName() + " has been enabled !");
+        //getServer().getPluginManager().registerEvents(new ChatListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // no need to do things here
+    }
+}
