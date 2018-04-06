4459a2649fbdbcdcd917c2b86e7330900549a30a
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2018-04-06 17:19:51 +0200

    Modify the format of display in the chat

--- src/main/java/fr/pinath/listener/ChatListener.java
+++ src/main/java/fr/pinath/listener/ChatListener.java
@@ -6,36 +6,43 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
+@SuppressWarnings("unused")
 public class ChatListener implements Listener {
+    private Scoreboard scoreboard;
+
+    public ChatListener() {
+        scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+    }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e) {
-        Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(e.getPlayer());
+        Team team = scoreboard.getEntryTeam(e.getPlayer().getDisplayName());
         if (team != null) {
             String teamColor = "§" + team.getColor().getChar();
-            e.setFormat("<" + teamColor + "[" + team.getDisplayName() + "] " + e.getPlayer().getName()
-                    + "§f> " + e.getMessage());
+            e.setFormat(teamColor + team.getDisplayName() + " " + e.getPlayer().getName()
+                    + "§7 :§f " + e.getMessage());
         }
     }
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(e.getPlayer());
+        Team team = scoreboard.getEntryTeam(e.getPlayer().getDisplayName());
         if (team != null) {
             String teamColor = "§" + team.getColor().getChar();
-            e.setJoinMessage(teamColor + "[" + team.getDisplayName() + "] " + e.getPlayer().getName()
+            e.setJoinMessage(teamColor + team.getDisplayName() + " " + e.getPlayer().getName()
                     + "§e joined the game");
         }
     }
 
     @EventHandler
     public void onLeft(PlayerQuitEvent e) {
-        Team team = Bukkit.getScoreboardManager().getMainScoreboard().getPlayerTeam(e.getPlayer());
+        Team team = scoreboard.getEntryTeam(e.getPlayer().getDisplayName());
         if (team != null) {
             String teamColor = "§" + team.getColor().getChar();
-            e.setQuitMessage(teamColor + "[" + team.getDisplayName() + "] " + e.getPlayer().getName()
+            e.setQuitMessage(teamColor + team.getDisplayName() + " " + e.getPlayer().getName()
                     + "§e left the game");
         }
     }
